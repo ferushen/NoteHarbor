bffb574637cd96e9445a09ff9d039ed9fe3999b0
ferushen <fbaka@list.ru>
2023-10-09 16:17:47 +0300

    Update appStore and rootReducer: add baseApi

--- src/app/appStore.ts
+++ src/app/appStore.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './rootReducer';
+import { baseApi } from '@/shared/api/baseApi';
 
 export const appStore = configureStore({
 	reducer: rootReducer,
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof appStore.getState>;
