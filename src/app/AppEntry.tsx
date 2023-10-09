1c6d3c693b03db08d48d7cb6dea22c5e066cfa28
ferushen <fbaka@list.ru>
2023-10-09 14:44:08 +0300

    Add AppEntry

---
+++ src/app/AppEntry.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './styles/index.css';
+
+const root = document.getElementById('root') as HTMLElement;
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    {'some component'}
+  </React.StrictMode>
+);
