f32a12a22efdc4ca769a710cbb151ec2a365e1f3
ferushen <fbaka@list.ru>
2023-10-11 18:04:08 +0300

    Fix base api

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,17 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const __API__ = 'https://moviesdatabase.p.rapidapi.com';
+const __API__ = 'http://swapi.dev/api/';
 
 export const baseApi = createApi({
 	reducerPath: 'api',
 	baseQuery: fetchBaseQuery({
 		baseUrl: __API__,
-		prepareHeaders: (headers) => {
-			headers.set('X-RapidAPI-Key', 'e96075d921mshf5291785b7b5145p11b185jsne984c90f9baa');
-			headers.set('X-RapidAPI-Host', 'moviesdatabase.p.rapidapi.com');
-
-			return headers;
-		},
 	}),
 	endpoints: () => ({}),
 });
