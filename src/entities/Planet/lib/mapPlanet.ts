63536d38ebc1d04b9edc392c4b719c13004c4bc7
ferushen <fbaka@list.ru>
2023-10-12 12:34:04 +0300

    Add planet api

---
+++ src/entities/Planet/lib/mapPlanet.ts
@@ -0,0 +1,25 @@
+import type { PlanetFromApi } from '../api/types';
+import type { Planet } from '../model/types';
+import { createMappedEntity, getIdFromUrl } from '@/shared/lib/helpers';
+
+export function mapPlanet(planet: PlanetFromApi): Planet {
+	const films = createMappedEntity(planet.films);
+	const persons = createMappedEntity(planet.residents);
+
+	const mappedPlanet = {
+		id: getIdFromUrl(planet.url),
+		name: planet.name,
+		diameter: planet.diameter,
+		rotationPeriod: planet.rotation_period,
+		orbitalPeriod: planet.orbital_period,
+		gravity: planet.gravity,
+		population: planet.population,
+		climate: planet.climate,
+		terrain: planet.terrain,
+		surfaceWater: planet.surface_water,
+		persons,
+		films,
+	};
+
+	return mappedPlanet;
+}
