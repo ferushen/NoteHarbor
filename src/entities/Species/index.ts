3e8e0687261aa6a5e9eb169109c0b5da563218ea
ferushen <fbaka@list.ru>
2023-10-12 12:38:12 +0300

    Add species public api

---
+++ src/entities/Species/index.ts
@@ -0,0 +1,3 @@
+export type { Species } from './model/types';
+export { useOneSpeciesQuery, useAllSpeciesQuery } from './api/speciesApi';
+export { SpeciesLink } from './ui/SpeciesLink/SpeciesLink';
