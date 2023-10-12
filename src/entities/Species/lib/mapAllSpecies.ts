e56542fdaee79b9b10ab50f39116192061896d1c
ferushen <fbaka@list.ru>
2023-10-12 12:37:45 +0300

    Add species api

---
+++ src/entities/Species/lib/mapAllSpecies.ts
@@ -0,0 +1,29 @@
+import type { SpeciesFromApi } from '../api/types';
+import type { Species } from '../model/types';
+import { createMappedEntity, getIdFromUrl } from '@/shared/lib/helpers';
+
+export function mapAllSpecies(species: SpeciesFromApi[]): Species[] {
+	const mappedSpecies = species.reduce<Species[]>((acc, race) => {
+		const films = createMappedEntity(race.films);
+		const persons = createMappedEntity(race.people);
+
+		const mappedRace = {
+			id: getIdFromUrl(race.url),
+			name: race.name,
+			classification: race.classification,
+			designation: race.designation,
+			averageHeight: race.average_height,
+			averageLifespan: race.average_lifespan,
+			eyeColors: race.eye_colors,
+			hairColors: race.hair_colors,
+			skinColors: race.skin_colors,
+			language: race.language,
+			homeWorld: race.homeworld,
+			persons,
+			films,
+		};
+		return [...acc, mappedRace];
+	}, []);
+
+	return mappedSpecies;
+}
